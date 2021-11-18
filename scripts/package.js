8f6dadd2cab776267f942f742886f9a73f8e84ff
Chance Hudson <jchancehud@gmail.com>
2021-11-17 22:25:57 -0600

    fix: copy readme into dist

--- scripts/package.js
+++ scripts/package.js
@@ -11,4 +11,16 @@ const lockpath = path.join(__dirname, '../package-lock.json')
 const newlockpath = path.join(__dirname, '../dist/package-lock.json')
 fs.copyFileSync(lockpath, newlockpath)
 
+const readmepath = path.join(__dirname, '../README.md')
+const newreadmepath = path.join(__dirname, '../dist/README.md')
+fs.copyFileSync(readmepath, newreadmepath)
+
+fs.writeFileSync(
+  path.join(__dirname, '../dist/.npmignore'),
+  `
+.DS_Store
+*.tmp
+*.swp
+  `)
+
 
